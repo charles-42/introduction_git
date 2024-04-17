0af76bd5348fec8cb8f18ef08ba51de597bc0df7
Charles Béniac <71760090+charles-42@users.noreply.github.com>
2024-04-17 14:06:37 +0200

    Update local.py

--- local.py
+++ local.py
@@ -5,3 +5,5 @@ print("modification qui vient de remote")
 print("nouveau changement depuis remote")
 
 print("nouveau changement en local") 
+
+print("modification sur local.py de la branche main en remote")
